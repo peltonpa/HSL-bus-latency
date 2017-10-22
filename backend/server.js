77e0ba541c0b9fd6900db773c6ae62c906e9d45d
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2017-10-22 18:41:30 +0300

    Try to load bundle and index from backend directory

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log(req);
-  res.sendfile(__dirname + "../frontend/dist/index.html");
+  res.sendfile(__dirname + "/dist/index.html");
 });
 
 app.listen(9001);
