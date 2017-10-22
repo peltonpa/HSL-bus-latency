5252ee889964eea9e454f0191de179b863645dca
Paavo <paavo@vultr.guest>
2017-10-22 17:46:17 +0000

    Express.js configured to serve the Leaflet app

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
   console.log(req);
-  res.sendfile(__dirname + "/dist/index.html");
+  res.sendFile("index.html");
+});
+
+app.get("/bundle.js", (req, res) => {
+  res.sendFile("bundle.js");
 });
 
 app.listen(9001);
