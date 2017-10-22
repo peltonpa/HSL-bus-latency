5ccb32042d79dae1a768c4497c239d1e4224725c
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2017-10-22 18:38:05 +0300

    Serve index.html from frontend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log(req);
-  res.send("Toimii");
+  res.sendfile(__dirname + "../frontend/dist/index.html");
 });
 
 app.listen(9001);
