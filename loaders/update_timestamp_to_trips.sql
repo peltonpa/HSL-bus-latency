908a842105269c2254dea574220bfe04a5ea5522
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2017-10-24 16:21:15 +0300

    And had wrong column name

--- loaders/update_timestamp_to_trips.sql
+++ loaders/update_timestamp_to_trips.sql
@@ -2,4 +2,4 @@ ALTER TABLE trips ADD COLUMN time integer;
 
 --SELECT gtfsid, substring(gtfsid, '....$') FROM trips;
 
-UPDATE trips SET timestring = (substring(gtfsid, '....$'))::integer; 
+UPDATE trips SET time = (substring(gtfsid, '....$'))::integer; 
