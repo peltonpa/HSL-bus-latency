0b2ee99b3178d7f78c5951ac8c62aa010bf96a9e
Paavo Peltonen <peltonpa@dhcp-asv-98.helsinki.fi>
2017-10-25 20:02:32 +0300

    Added Webpack loaders for Blueprint fonts

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -37,7 +37,26 @@ const config = {
             },
           },
         ],
-      }
+      }, {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[hash].[ext]',
+            limit: 5000,
+            mimetype: 'application/font-woff'
+          }
+        }
+      }, 
+      {
+        test: /\.(ttf|eot|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[hash].[ext]'
+          }
+        }
+      },
     ]
   }
 };
